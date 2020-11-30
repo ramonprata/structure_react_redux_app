86caac451edccb5e3b05cb77be6e016484f79423
Ramon Prata <pratara@contractor.rhimagnesita.com>
2020-11-30 12:33:13 -0300

    Update README

--- src/users/userService.js
+++
@@ -1,6 +0,0 @@
-class UserService {
-  async getUsers(filter) {
-    const URI = `${BASE_URL}/users&filter=${filter}`;
-    return axios.get(URI);
-  }
-}
--- src/users/userUtils.js
+++
@@ -1,3 +0,0 @@
-export const getFilterUsersByName = (textFilter) => {
-  return '';
-};
